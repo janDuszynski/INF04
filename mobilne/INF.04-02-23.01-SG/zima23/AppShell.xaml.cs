7911c7599d306f2245e1cef8c34ab14b8c1c7f06
janDuszynski <jasiek.duszynski@gmail.com>
2025-01-14 22:40:36 +0100

    feat: zima23

---
+++ mobilne/INF.04-02-23.01-SG/zima23/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace zima23
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ mobilne/INF.04-02-23.01-SG/zima23/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace zima23
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Lista = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ItemsList.ItemsSource = Lista;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (EntryField.Text != "")
+            {
+                Lista.Add(EntryField.Text.ToString());
+                EntryField.Text = string.Empty;
+            }
+        }
+    }
+
+}
